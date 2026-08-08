a6c25e03eb9e89760884cf96e02d99c312a3231d
Sandy <sandilyamachiraju33@gmail.com>
2026-08-08 11:03:22 +0530

    Add chatmodel_huggingface.py and update llmdemo.py for model improvements

--- 1.Models/llmdemo.py
+++ 1.Models/llmdemo.py
@@ -1,10 +1,10 @@
 import os
 from langchain.chat_models import init_chat_model
-from dotenv  import load_dotenv
+from dotenv import load_dotenv
 
 load_dotenv()
 
-model = init_chat_model("google_genai:gemini-2.5-flash-lite")
+model = init_chat_model("google_genai:gemini-3.5-flash-lite")
 
-result=model . invoke("What is the Capital of India")
-print(result)
+result = model.invoke("What is the Capital of India")
+print(result.content)
